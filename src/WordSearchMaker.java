cfd07e2780341d5770f16548d6c03e46a2ec8def
NateClarkTech <nateclarktech@gmail.com>
2023-04-28 11:13:59 -0400

    Made starter classes.

---
+++ src/WordSearchMaker.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class WordSearchMaker {
+    private ArrayList<String> words;
+
+
+    ArrayList<String> getWords(){
+        return words;
+    }
+    void setWords(){
+
+    }
+}
