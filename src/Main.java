7a4d461802a02f3c05736fbe9c4b889d8555b618
NateClarkTech <nateclarktech@gmail.com>
2023-05-02 16:39:15 -0400

    Made starter classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Imagine");
+        WordSearchMaker wordSearch = new WordSearchMaker(words);
+        wordSearch.makeWordSearch();
+        wordSearch.printBoard();
+        System.out.println();
+        wordSearch.makeWordSearch(words);
+        wordSearch.printBoard();
     }
 }
--- src/WordSearchMaker.java
+++ src/WordSearchMaker.java
@@ -2,12 +2,47 @@ import java.util.ArrayList;
 
 public class WordSearchMaker {
     private ArrayList<String> words;
+    private ArrayList<ArrayList<Character>> board;
 
+    WordSearchMaker(){
+        this.words = new ArrayList<String>();
+        this.board = new ArrayList<ArrayList<Character>>();
+    }
+
+    WordSearchMaker(ArrayList<String> words){
+        this.words.addAll(words);
+        this.board = new ArrayList<ArrayList<Character>>();
+    }
 
     ArrayList<String> getWords(){
         return words;
     }
-    void setWords(){
+    void setWords(ArrayList<String> words){
+        this.words.addAll(words);
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < board.size(); i++){
+            for (int j = 0; j < board.get(i).size(); j++){
+
+            }
+        }
+    }
+
+    public void makeWordSearch(ArrayList<String> words) {
+        makeWordSearchBoard();
+    }
+
+    public void makeWordSearch() {
+        makeWordSearchBoard();
+
+
+
+
+    }
+
+    private void makeWordSearchBoard(){
+
         //add code here
     }
 }
