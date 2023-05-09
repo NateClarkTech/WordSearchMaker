81b3f50e0bf7d83030bb7e8db302de6e533120f3
NateClarkTech <nateclarktech@gmail.com>
2023-05-09 14:14:01 -0400

    Inserts words but has slight bug.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<String>();
-        words.add("HELLO");
-        words.add("GAMERS");
+        words.add("BLACKSMITH");
+        words.add("SCREWDRIVER");
+        words.add("NAVIGATION");
+        words.add("RENDER");
         WordSearchMaker wordSearch = new WordSearchMaker(words);
         wordSearch.makeWordSearch();
         wordSearch.printBoard();
